24d1920d0c606eece5229ba732f1aa8b2fd753fd
EduardoFernandes1410 <eduardo.militao1410@gmail.com>
2019-11-22 14:28:21 -0300

    Final

--- paths.py
+++ paths.py
@@ -1,10 +1,18 @@
 import os
-root_folder = "./20_newsgroups"
-folders = os.listdir(root_folder)
+import glob
+root_folder = "./dataset"
 f = open("input.txt", "w")
+folders = os.listdir(root_folder)
+root_files = glob.glob(os.path.join(root_folder, "*"))
+for file in root_files:
+	if( not os.path.isdir(file) ):
+		print(file)
+		f.write("."+os.path.join(file) + "\n")
+
 for folder in folders:
-    arquivos = os.listdir(os.path.join(root_folder, folder))
-    for arquivo in arquivos:
-        print(arquivo)
-        f.write((os.path.join("." + root_folder,folder, arquivo) + "\n"))
+	if(os.path.isdir(os.path.join(root_folder, folder))):
+		arquivos = glob.glob(os.path.join(root_folder, folder, "*"))
+		for arquivo in arquivos:
+			print(arquivo)
+			f.write("."+ arquivo + "\n")
 
